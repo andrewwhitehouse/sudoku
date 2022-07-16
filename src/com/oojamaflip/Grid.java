3ba2e8a5072c7ff6439ecbc0b7e2c3da5d61cb3d
Andrew Whitehouse <andrew@whitehouse.org.uk>
2022-07-16 11:37:53 +0100

    Partially working solution

--- src/com/oojamaflip/Grid.java
+++ src/com/oojamaflip/Grid.java
@@ -1,5 +1,8 @@
 package com.oojamaflip;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Grid {
     private int verticalBoxes;
     private int horizontalBoxes;
@@ -10,24 +13,108 @@ public class Grid {
         this.verticalBoxes = verticalBoxes;
         this.horizontalBoxes = horizontalBoxes;
         this.boxSize = boxSize;
-        values = new int[(verticalBoxes*boxSize)*(horizontalBoxes*boxSize)];
+        initializeValues();
+
+    }
+
+    private void initializeValues() {
+        values = new int[rowCount() * columnCount()];
+    }
+
+    public int getValue(int rowIndex, int colIndex) {
+        return values[rowIndex * columnCount() + colIndex];
+    }
+
+    private void setValue(int rowIndex, int colIndex, int value) {
+        values[rowIndex * columnCount() + colIndex] = value;
+    }
+
+    public int rowCount() {
+        return verticalBoxes * boxSize;
+    }
+
+    public int columnCount() {
+        return horizontalBoxes * boxSize;
+    }
+
+    public static Grid copy(Grid grid) {
+        Grid newGrid = new Grid(grid.verticalBoxes, grid.horizontalBoxes, grid.boxSize);
+        System.arraycopy(grid.values, 0, newGrid.values, 0, grid.values.length);
+        return newGrid;
     }
 
     public String toString() {
         StringBuffer buffer = new StringBuffer();
-        for(int rowIndex=0; rowIndex < verticalBoxes*boxSize; rowIndex++) {
-            for(int colIndex=0; colIndex < horizontalBoxes*boxSize; colIndex++) {
-                buffer.append(values[rowIndex*colIndex]);
-                buffer.append(colIndex == horizontalBoxes*boxSize-1 ? "\n" : "  ");
+        int rowLength = horizontalBoxes * boxSize;
+        for (int rowIndex = 0; rowIndex < verticalBoxes * boxSize; rowIndex++) {
+            for (int colIndex = 0; colIndex < horizontalBoxes * boxSize; colIndex++) {
+                buffer.append(values[rowIndex * rowLength + colIndex]);
+                buffer.append(colIndex == horizontalBoxes * boxSize - 1 ? "\n" : "  ");
             }
         }
         return buffer.toString();
     }
 
-//    public Grid populate(int boxIndex, int value, IntSelector selector) {
-//        Grid newGrid = new Grid(verticalBoxes, horizontalBoxes, boxSize);
-//        int topLeftColumnIndex = boxIndex*horizontalBoxes*boxIndex;
-//        int topLeftRowIndex = boxIndex*verticalBoxes*boxIndex;
-//        newGrid.values[]
-//    }
+    private boolean columnContainsValue(int colIndex, int value) {
+        for (int rowIndex = 0; rowIndex < rowCount(); rowIndex++) {
+            if (getValue(rowIndex, colIndex) == value) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean rowContainsValue(int rowIndex, int value) {
+        for (int colIndex = 0; colIndex < columnCount(); colIndex++) {
+            if (getValue(rowIndex, colIndex) == value) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public Grid populate(int boxIndex, int value) {
+        int topLeftColumnIndex = (boxIndex % horizontalBoxes) * boxSize;
+        int topLeftRowIndex = (boxIndex / verticalBoxes) * boxSize;
+        List<Position> availablePositions = new ArrayList();
+        for (int rowIndex = 0; rowIndex < boxSize; rowIndex++) {
+            for (int colIndex = 0; colIndex < boxSize; colIndex++) {
+                int tmpValue = getValue(rowIndex + topLeftRowIndex,
+                        colIndex + topLeftColumnIndex);
+                if (tmpValue == 0
+                        && !columnContainsValue(colIndex+topLeftColumnIndex, value)
+                        && !rowContainsValue(rowIndex+topLeftRowIndex, value)) {
+                    availablePositions.add(new Position(rowIndex+topLeftRowIndex,
+                            colIndex+topLeftColumnIndex));
+                }
+            }
+        }
+        if (availablePositions.isEmpty()) {
+            throw new IllegalStateException(
+                    String.format("No position available for value %d in boxIndex %d grid %s",
+                            value, boxIndex, toString()));
+        }
+        Grid newGrid = copy(this);
+        Position firstAvailable = availablePositions.get(0);
+        newGrid.setValue(firstAvailable.getRow(), firstAvailable.getColumn(), value);
+        return newGrid;
+    }
+
+    static class Position {
+        private int row;
+        private int column;
+
+        public Position(int row, int column) {
+            this.row = row;
+            this.column = column;
+        }
+
+        public int getRow() {
+            return row;
+        }
+
+        public int getColumn() {
+            return column;
+        }
+    }
 }
--- src/com/oojamaflip/GridTest.java
+++ src/com/oojamaflip/GridTest.java
@@ -7,11 +7,67 @@ public class GridTest {
     @Test
     void shouldGenerateGrid() {
         Grid grid = new Grid(2, 2, 2);
-        String expected =
-                "0  0  0  0\n" +
-                "0  0  0  0\n" +
-                "0  0  0  0\n" +
-                "0  0  0  0\n";
-        Assertions.assertEquals(expected, grid.toString());
+        int[][] expected = new int[][]{
+                {0, 0, 0, 0},
+                {0, 0, 0, 0},
+                {0, 0, 0, 0},
+                {0, 0, 0, 0}
+        };
+        Assertions.assertArrayEquals(expected, toArray(grid));
+    }
+
+    @Test
+    void shouldPopulateFirstAndSecondBoxes() {
+        Grid grid = new Grid(2, 2, 2)
+                .populate(0, 1)
+                .populate(1, 1);
+
+        int[][] expected = new int[][]{
+                {1, 0, 0, 0},
+                {0, 0, 1, 0},
+                {0, 0, 0, 0},
+                {0, 0, 0, 0}
+        };
+        Assertions.assertArrayEquals(expected, toArray(grid));
+    }
+
+    @Test
+    void shouldPopulateAllBoxes() {
+        Grid grid = new Grid(2, 2, 2)
+                .populate(0, 1)
+                .populate(1, 1)
+                .populate(2, 1)
+                .populate(3, 1);
+
+        int[][] expected = new int[][]{
+                {1, 0, 0, 0},
+                {0, 0, 1, 0},
+                {0, 1, 0, 0},
+                {0, 0, 0, 1}
+        };
+        System.out.println(grid.toString());
+        Assertions.assertArrayEquals(expected, toArray(grid));
+    }
+
+    @Test
+    void shouldPopulateNineByNineGrid() {
+        Grid grid = new Grid(3, 3, 3);
+        for(int value=1; value <= 9; value++) {
+            for(int boxIndex=0; boxIndex < 9; boxIndex++) {
+                grid = grid.populate(boxIndex, value);
+            }
+        }
+        System.out.println(grid.toString());
+        Assertions.assertEquals(1, grid.getValue(0, 0));
+    }
+
+    private int[][] toArray(Grid grid) {
+        int ret[][] = new int[grid.rowCount()][grid.columnCount()];
+        for(int rowIndex=0; rowIndex < grid.rowCount(); rowIndex++) {
+            for(int colIndex=0; colIndex< grid.columnCount(); colIndex++) {
+                ret[rowIndex][colIndex] = grid.getValue(rowIndex, colIndex);
+            }
+        }
+        return ret;
     }
 }
